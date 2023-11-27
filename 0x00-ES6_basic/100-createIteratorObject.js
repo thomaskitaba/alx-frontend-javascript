5e9a6a2ce5df1c24fe104ff6a885eddc1acac501
thomaskitaba <thomaskitaba@gmail.com>
2023-11-27 20:51:46 +0300

    all

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,10 @@
 export default function createIteratorObject(report) {
-
+  let allEmp = [];
+  for (const item of Object.values(report.allEmp)) {
+    allEmp = [
+      ...allEmp,
+      ...item,
+    ];
+  }
+  return allEmp;
 }
