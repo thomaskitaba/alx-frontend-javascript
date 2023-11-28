6c620f18c5d326298f384eb60e0564d68df9103f
thomaskitaba <thomaskitaba@gmail.com>
2023-11-28 13:01:15 +0300

    0-promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,7 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    if (true) {
+    const success = true;
+    if (success) {
       resolve();
     } else {
       reject();
