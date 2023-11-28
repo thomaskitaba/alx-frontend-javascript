31374a0822d3a690fb058853449d85288c11a207
thomaskitaba <thomaskitaba@gmail.com>
2023-11-28 21:42:02 +0300

    npm run dev 4-main.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   const data = {
     status: 200,
-    body: 'success'
+    body: 'success',
   };
   return promise
     .then(() => (data))
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
+// question what is the diffrence bwet
