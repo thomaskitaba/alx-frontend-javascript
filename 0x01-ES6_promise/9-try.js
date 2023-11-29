7f83605e5b3a237f2af4dde8571cc1ed23017678
thomaskitaba <thomaskitaba@gmail.com>
2023-11-29 14:21:09 +0300

    9-try.js using stringify instad of toString()

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -6,11 +6,12 @@ export default function guardrail(mathFunction) {
     // since error object is enumerable we have to serialize them
     // first
     const errorData = {
-    name: error.name,
-    message: error.message,
-    stack: error.stack,
+      name: error.name,
+      message: error.message,
+      stack: error.stack,
     };
-    queue.push(JSON.parse(JSON.stringify(errorData)).name + ': ' + JSON.parse(JSON.stringify(errorData)).message);
+    queue.push(`${JSON.parse(JSON.stringify(errorData)).name}: ${JSON.parse(JSON.stringify(errorData)).message}`);
+    // or we can just use error.toString()
   } finally {
     queue.push('Guardrail was processed');
   }
