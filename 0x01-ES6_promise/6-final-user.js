b1628e032efb0f84a999af2b8add7bed57ea0205
thomaskitaba <thomaskitaba@gmail.com>
2023-11-29 13:04:40 +0300

    6-final-user.js_corrected_version

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const data = await Promise.allSettled([
-        signUpUser(firstName, lastName),
-        uploadPhoto(fileName),
-    ]);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((data) => {
     const result = [];
     for (const row of data) {
-        result.push({ status: row.status, value: data.value || data.reason });
+      result.push({ status: row.status, value: row.value || row.reason });
     }
     return result;
+  });
 }
