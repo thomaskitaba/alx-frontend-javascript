3ad79a18a89de134b10ff6e0cd22970c8e670bce
thomaskitaba <thomaskitaba@gmail.com>
2023-11-29 15:23:16 +0300

    6-allSettled.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]).then((data) => {
