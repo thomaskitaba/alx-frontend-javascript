786263cac09eab1bbddce0843985f82e25896fdc
thomaskitaba <thomaskitaba@gmail.com>
2023-11-29 12:47:45 +0300

    6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([
+    signUpUser(`${firstName}`, `${lastName}`),
+    uploadPhoto(`${fileName}`),
+  ]).then((data) => {
+    const result = [];
+    for (const row of data) {
+      result.push({ status: row.status, value: data.value || data.reason });
+    }
+    return result;
+  });
+}
+// NB;   returned data  by promise.all
+// [
+//     { status: 'fulfilled', value: { firstName: 'Bob', lastName: 'Dylan' } },
+//     { status: 'rejected', reason: Error: bob_dylan.jpg cannot be processed at uploadPhoto ... }
+//   ]
