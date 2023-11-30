f050cbf981101e4e3fd6e70351a70822d90709ef
thomaskitaba <thomaskitaba@gmail.com>
2023-11-30 11:15:46 +0300

    inseption of pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" return list of integers representing
+the pascals triangle of n """
+
+
+def pascal_triangle(n)
+""" return list of integers """
+
+	if n <= 0:
+  	return []
