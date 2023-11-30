75afb08d6d75f9e7aa9279267549db2324ee22c5
thomaskitaba <thomaskitaba@gmail.com>
2023-11-30 16:54:24 +0300

    10-cars

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,23 +1,25 @@
 export default class Car {
-    constructor(brand, motor, color) {
-      if (typeof brand !== 'string') {
-        throw new TypeError('brand must be a string');
-      }
-      if (typeof motor !== 'string') {
-        throw new TypeError('motor must be a string');
-      }
-      if (typeof color !== 'string') {
-        throw new TypeError('color must be a string');
-      }
-      this._brand = brand;
-      this._motor = motor;
-      this._color = color;
+  constructor(brand, motor, color) {
+    if (typeof brand !== 'string') {
+      throw new TypeError('brand must be a string');
     }
-    static get [Symbol.species]() {
-        return this;
+    if (typeof motor !== 'string') {
+      throw new TypeError('motor must be a string');
     }
-    cloneCar() {
-        const NewCar = this.constructor[Symbol.species];
-        return new NewCar();
+    if (typeof color !== 'string') {
+      throw new TypeError('color must be a string');
     }
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const NewCar = this.constructor[Symbol.species];
+    return new NewCar();
+  }
+}
