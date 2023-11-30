1806719538e2409eac005106f0ebdc28225a2d23
thomaskitaba <thomaskitaba@gmail.com>
2023-11-30 15:26:16 +0300

    building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,19 +1,22 @@
-export default class Building{
-    constuctor (sqft) {
-        if (typeof this.evacuationWarningMessage !== "function") {
-            throw new Error("Class extending Building must override evacuationWarningMessage");
-        }
-        if (typeof sqft !== "number") {
-            throw new TypeError("Sqft must be number");
-        }
-        this._sqft = sqft;
+export default class Building {
+  constuctor(sqft) {
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    get sqft(){
-        return this._sqft;
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be number');
     }
-    set sqft(sqft){
-        if (typeof sqft !== "number"){
-            throw new TypeError("sqft must be number");
-        }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be number');
     }
+    this._sqft = sqft;
+  }
 }
