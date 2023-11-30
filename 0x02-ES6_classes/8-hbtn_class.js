6d1398687394e737447e0ab5dba20f9c14fc6617
thomaskitaba <thomaskitaba@gmail.com>
2023-11-30 16:28:04 +0300

    topremitive

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,19 @@
+ export default class HolbertonClass{
+    constructor(size, location) {
+        if (typeof size !== "number") {
+            throw new TypeError("Size must be a number");
+        }
+        if (typeof location !== "string") {
+            throw new TypeError("location must be a string");
+        }
+        this._size = size;
+        this._location = location;
+    }
+    get [Symbol.toPrimitive](hint) {
+        if (hint == 'number') {
+            return this._size;
+        }
+        return this._location;
+    }
+
+}
