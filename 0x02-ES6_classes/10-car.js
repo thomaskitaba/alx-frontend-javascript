ea2ac55c3d8e2ee9d4982a9d4b885c928010c4ed
thomaskitaba <thomaskitaba@gmail.com>
2023-11-30 16:50:36 +0300

    clonecar

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,23 @@
+export default class Car {
+    constructor(brand, motor, color) {
+      if (typeof brand !== 'string') {
+        throw new TypeError('brand must be a string');
+      }
+      if (typeof motor !== 'string') {
+        throw new TypeError('motor must be a string');
+      }
+      if (typeof color !== 'string') {
+        throw new TypeError('color must be a string');
+      }
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
+    }
+    static get [Symbol.species]() {
+        return this;
+    }
+    cloneCar() {
+        const NewCar = this.constructor[Symbol.species];
+        return new NewCar();
+    }
+  }
