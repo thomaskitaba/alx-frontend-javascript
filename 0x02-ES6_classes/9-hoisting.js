4273d29405b6ebfc8c72c4be7f7bab6ccec62fdf
thomaskitaba <thomaskitaba@gmail.com>
2023-11-30 17:38:49 +0300

    9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -18,11 +18,12 @@ export default class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-export class StudentHolberton {
-  constructor(firstName, lastName, holbertonClass) {
-    this._firstName = firstName;
+export class StudentHolberton extends HolbertonClass{
+  constructor(year, location, firstName, lastName, holbertonClass) {
+    super(year, location);
+		this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass;
+    // this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
