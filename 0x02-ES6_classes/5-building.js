25670c03df6fb587d48f46b87738c8f0f953fd77
thomaskitaba <thomaskitaba@gmail.com>
2023-11-30 15:32:50 +0300

    building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,5 @@
 export default class Building {
-    constructor(sqft) {
+  constructor(sqft) {
     if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
