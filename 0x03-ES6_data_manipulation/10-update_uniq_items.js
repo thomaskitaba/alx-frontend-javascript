34bc442cde21a68f631bdb87cbb9a6839b33df31
thomaskitaba <thomaskitaba@gmail.com>
2023-12-04 23:32:51 +0300

    10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,10 @@
 export default function updateUniqueItems(myMap) {
-    if (!(myMap instanceof Map)) {
-        throw Error("Cannot process");
+  if (!(myMap instanceof Map)) {
+    throw Error('Cannot process');
+  }
+  for (const [key, value] of myMap) {
+    if (value === 1) {
+      myMap.set(key, 100);
     }
-    for (const [key, value] of myMap) {
-        if (value === 1) {
-            myMap.set(key, 100);
-        }
-    }
+  }
 }
