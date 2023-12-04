099b311a8a094d175b7417326ca9752423db0ea9
thomaskitaba <thomaskitaba@gmail.com>
2023-12-04 17:11:08 +0300

    7-has_array

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,19 @@
 function hasValuesFromArray(mySet, value) {
-  return value.every((obj) => mySet.has(obj));
+//   return value.every((obj) => mySet.has(obj));
+let ans = 1;
+for (const row in value) {
+    if (!mySet.has(row)){
+        ans = 0;
+        break;
+    }
+    else {
+        ans = 1;
+    }
+}
+if (ans === 1) {
+    return true;
+} else {
+    return false;
+}
 }
 export default hasValuesFromArray;
