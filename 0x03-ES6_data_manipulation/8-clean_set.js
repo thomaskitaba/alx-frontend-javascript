5705dd770eb45b21962c4efc6d66891f7a49fa2a
thomaskitaba <thomaskitaba@gmail.com>
2023-12-04 22:05:27 +0300

    8-clean-corrected-with-only-one-error

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,36 +1,36 @@
- function cleanSet(set, startString) {
-    let myArray = [];
+function cleanSet(set, startString) {
+  const myArray = [];
 
-    for (const row of set) {
-        // console.log(row);
-        const stringArry = [];
-        let answer = 1;
-        if (startString.length < row.length)
-            for (let i = 0; i < row.length; i++) {
-                // console.log(row[i]);
-                if (row[i] == startString[i] && i < 3) {
-                    answer = 1;
-                    // console.log(row[i]);
-                    continue;
-                }
-                if (row[i] != startString[i] && i < 3) {
-                    answer = 0;
-                    break;
-                }
+  for (const row of set) {
+    // console.log(row);
+    const stringArry = [];
+    let answer = 1;
+    if (startString.length < row.length) {
+      for (let i = 0; i < row.length; i += 1) {
+      // console.log(row[i]);
+        if (row[i] === startString[i] && i < 3) {
+          answer = 1;
+          // console.log(row[i]);
+          continue;
+        }
+        if (row[i] !== startString[i] && i < 3) {
+          answer = 0;
+          break;
+        }
 
-                if (i >= 3) {
-                    // start appending
-                    // console.log(row[i]);
-                    stringArry.push(row[i]);
-
-                }
-            }
-            if (answer === 1) {
-                // console.log(stringArry);
-                myArray.push(stringArry.join(''));
-            }
+        if (i >= 3) {
+        // start appending
+        // console.log(row[i]);
+          stringArry.push(row[i]);
+        }
+      }
     }
+    if (answer === 1) {
+      // console.log(stringArry);
+      myArray.push(stringArry.join(''));
+    }
+  }
 
-    console.log(myArray.join('-'));
- }
- cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon');
+  return myArray.join('-');
+}
+export default cleanSet;
