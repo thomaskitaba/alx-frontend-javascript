ac410918dd658d6a41058f3fc66472352eabc837
thomaskitaba <thomaskitaba@gmail.com>
2023-12-04 22:39:53 +0300

    update-eslint

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -9,31 +9,23 @@ function cleanSet(set, startString) {
   }
 
   for (const row of set) {
-    // console.log(row);
     const stringArry = [];
     let answer = 1;
     if (startString.length < row.length) {
       for (let i = 0; i < row.length; i += 1) {
-      // console.log(row[i]);
         if (row[i] === startString[i] && i < 3) {
           answer = 1;
-          // console.log(row[i]);
-        //   continue;
         }
         if (row[i] !== startString[i] && i < 3) {
           answer = 0;
           break;
         }
-
         if (i >= 3) {
-        // start appending
-        // console.log(row[i]);
           stringArry.push(row[i]);
         }
       }
     }
     if (answer === 1) {
-      // console.log(stringArry);
       myArray.push(stringArry.join(''));
     }
   }
