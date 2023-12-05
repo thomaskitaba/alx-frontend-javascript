487a54eff4bcbc2939d058d9a3de648e46d2461f
thomaskitaba <thomaskitaba@gmail.com>
2023-12-05 14:49:10 +0300

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -11,7 +11,7 @@ function cleanSet(set, startString) {
   // loop accross the set and get each row
   for (const row of set) {
     // check if each row starts with the startString
-    if (row.startsWith(startString)) {
+    if (row && row.startsWith(startString)) {
       myArray.push(row.slice(startString.length));
     }
   }
