697c6cab3a1b212adfec4af2badeb83463bb3212
thomaskitaba <thomaskitaba@gmail.com>
2023-12-05 14:50:53 +0300

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -8,13 +8,29 @@ function cleanSet(set, startString) {
   }
 
   const myArray = [];
-  // loop accross the set and get each row
+
   for (const row of set) {
-    // check if each row starts with the startString
-    if (row && row.startsWith(startString)) {
-      myArray.push(row.slice(startString.length));
+    if (row && startString.length < row.length) {
+      let answer = 1;
+      const stringArray = [];
+
+      for (let i = 0; i < row.length; i += 1) {
+        if (i < 3) {
+          if (row[i] !== startString[i]) {
+            answer = 0;
+            break;
+          }
+        } else {
+          stringArray.push(row[i]);
+        }
+      }
+
+      if (answer === 1) {
+        myArray.push(stringArray.join(''));
+      }
     }
   }
+
   return myArray.join('-');
 }
 
---
+++ 0x03-ES6_data_manipulation/8-clean_set_working.js
@@ -0,0 +1,21 @@
+function cleanSet(set, startString) {
+  if (
+    typeof set !== 'object'
+      || typeof startString !== 'string'
+      || startString.length === 0
+  ) {
+    return '';
+  }
+
+  const myArray = [];
+  // loop accross the set and get each row
+  for (const row of set) {
+    // check if each row starts with the startString
+    if (row && row.startsWith(startString)) {
+      myArray.push(row.slice(startString.length));
+    }
+  }
+  return myArray.join('-');
+}
+
+export default cleanSet;
