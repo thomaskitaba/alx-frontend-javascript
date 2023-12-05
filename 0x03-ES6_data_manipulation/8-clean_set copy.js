e26b8c58e4bb855db423ce533bc9b042df60bafb
thomaskitaba <thomaskitaba@gmail.com>
2023-12-05 14:12:18 +0300

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set copy.js
+++ 0x03-ES6_data_manipulation/8-clean_set copy.js
@@ -12,8 +12,7 @@ function cleanSet(set, startString) {
   for (const row of set) {
     if (startString.length < row.length) {
       let answer = 1;
-      let count = 0;
-      let stringArray = [];
+      const stringArray = [];
 
       for (let i = 0; i < row.length; i += 1) {
         if (i < 3) {
@@ -22,17 +21,12 @@ function cleanSet(set, startString) {
             break;
           }
         } else {
-            count = count + 1;
-            if (count === startString.length) {
-                stringArray = row.slice(count);
-                break;
-            }
-        //   stringArray.push(row[i]);
+          stringArray.push(row[i]);
         }
       }
 
       if (answer === 1) {
-        myArray.push(stringArray);
+        myArray.push(stringArray.join(''));
       }
     }
   }
