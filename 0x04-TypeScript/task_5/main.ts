f1caf8d0d6e068915894439aa974db11be753ccc
thomaskitaba <thomaskitaba@gmail.com>
2023-12-06 21:24:07 +0300

    update

--- 0x04-TypeScript/task_5/main.ts
+++ 0x04-TypeScript/task_5/main.ts
@@ -7,9 +7,9 @@ interface MinorCredits {
     brand: "Minor";
 }
 
-function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits){
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): number{
     return subject1.credit + subject2.credit;
 }
-function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits){
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): number{
     return subject1.credit + subject2.credit;
 }
